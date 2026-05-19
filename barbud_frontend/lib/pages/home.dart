9f0e163f266f166c2ef1aaf8adc19c4d0553d1b1
WojtekZb <554101@student.fontys.nl>
2026-05-19 09:17:36 +0200

    To test storage made it display welcome and the username

--- barbud_frontend/lib/pages/home.dart
+++ barbud_frontend/lib/pages/home.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const storage = FlutterSecureStorage();
+
+  String? username;
+
+  @override
+  void initState() {
+    super.initState();
+    loadUsername();
+  }
+
+  Future<void> loadUsername() async {
+    final storedUsername = await storage.read(key: 'username');
+
+    setState(() {
+      username = storedUsername;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,19 +33,12 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                "HomePage works",
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.black,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-            ],
+          child: Text(
+            "Welcome ${username ?? 'user'}",
+            style: const TextStyle(
+              fontSize: 28,
+              color: Colors.black,
+            ),
           ),
         ),
       ),
